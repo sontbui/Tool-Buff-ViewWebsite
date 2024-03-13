407641d6ec539e9bb347c6c3fd79e516ff2b54a9
Bui Thanh Son <106475239+sontbui@users.noreply.github.com>
2024-03-13 21:07:54 +0700

    Update WebPageLoader.java

--- src/WebPageLoader.java
+++ src/WebPageLoader.java
@@ -1,16 +1,13 @@
 import java.awt.Desktop;
 import java.net.URI;
-
 public class WebPageLoader {
     public static void main(String[] args) {
         // URL của trang web bạn muốn tải
         String url = "https://github.com/sontbui";
-
         try {
             // Tạo đối tượng Desktop
             Desktop desktop = Desktop.getDesktop();
             int i = 2780;
-
             // Mở trang web trong trình duyệt mặc định của hệ thống
             while(true){
                 desktop.browse(new URI(url));
