ba89cc91b076db3f4744449a2d33e990fa7431ee
sontbui <sonbuithanh306@gmail.com>
2024-03-13 12:39:22 +0700

    new updating on 13-03

--- src/WebPageLoader.java
+++ src/WebPageLoader.java
@@ -2,24 +2,27 @@ import java.awt.Desktop;
 import java.net.URI;
 
 public class WebPageLoader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
         // URL của trang web bạn muốn tải
         String url = "https://github.com/sontbui";
 
         try {
             // Tạo đối tượng Desktop
             Desktop desktop = Desktop.getDesktop();
-            int i = 0;
+            // Số lượng view hiện tại - chưa tìm được cách lấy view từ github về nên tự cập nhật trước khi chạy
+            int i = 2271;
 
             // Mở trang web trong trình duyệt mặc định của hệ thống
             while(true){
                 
                 desktop.browse(new URI(url));
                 Thread.sleep(1500);
+                
                 System.out.println(i++);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    
 }
